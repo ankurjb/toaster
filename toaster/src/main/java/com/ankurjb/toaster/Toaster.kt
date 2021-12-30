eccdb5363957b66589e99d6fc00e4260f021a505
Ankur Singh <ankursinghjb@gmail.com>
2021-12-30 10:50:41 +0530

    new commit

--- toaster/src/main/java/com/ankurjb/toaster/Toaster.kt
+++ toaster/src/main/java/com/ankurjb/toaster/Toaster.kt
@@ -7,4 +7,8 @@ object Toaster {
     fun toast(c: Context, string: String) {
         Toast.makeText(c, string, Toast.LENGTH_SHORT).show()
     }
+
+    fun String.toast(c: Context) {
+        Toast.makeText(c, this, Toast.LENGTH_SHORT).show()
+    }
 }
